94183b6dd225d7bb2d74651c652c8128d1ee943e
Carter <carterinmotion@gmail.com>
2025-10-19 18:06:33 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Booklore inputs";
+
+  inputs = {
+    build-gradle-application.url = "github:raphiz/buildGradleApplication";
+  };
+
+  outputs = { self, nixpkgs, build-gradle-application, ... }:
+    let
+      system = "x86_64-linux";
+	  pkgs = import nixpkgs {
+        inherit system;
+        overlays = [build-gradle-application.overlays.default];
+      };
+    in {
+	  packages.${system} = {
+	    booklore-api = pkgs.callPackage ./booklore-api.nix { };
+      };
+	  # nixosModules.booklore-api = import ./module/booklore-api.nix;
+    };
+}
